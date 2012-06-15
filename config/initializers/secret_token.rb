d0082b150817273519b4f64b57ce65a18290c915
Adam Robbie <adam.robbie@gmail.com>
2012-06-15 09:13:37 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artemis::Application.config.secret_token = '0988a1d0b779ace43d080185e1709745516bb08124a439c894bed26f8ba4389a7cee2b05b00eb93a9c879368b522a8c3f265e60abc7cc480249944411fd90cc3'
